096d4553cc93696d9561c30a98efcb1ffb42158d
wellifabio <wellifabio@hotmail.com>
2026-09-11 16:23:35 -0300

    corrigido adaptador

--- src/data/prisma.js
+++ src/data/prisma.js
@@ -1,7 +1,9 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+// const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const { PrismaPg } = require("@prisma/adapter-pg");
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+// const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 const prisma = new PrismaClient({ adapter });
 
