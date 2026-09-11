e2607f960663e6bd08798d1e55025f8b1de59ec4
wellifabio <wellifabio@hotmail.com>
2026-09-11 17:02:33 -0300

    rotas

--- api/index.js
+++ api/index.js
@@ -1,32 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const estadiaRoutes = require('../src/routes/estadia.routes');
-app.use('/estadia', estadiaRoutes);
+const estadiaRoutes = require("../src/routes/estadia.routes");
+app.use("/estadia", estadiaRoutes);
 
-const veiculoRoutes = require('../src/routes/veiculo.routes');
-app.use('/veiculo', veiculoRoutes);
+const veiculoRoutes = require("../src/routes/veiculo.routes");
+app.use("/veiculo", veiculoRoutes);
 
-app.get('/', (req, res) => {
-  res.json({ message: 'API estacionamento online',rotas:[
-    '/':'GET',
-    '/veiculo/cadastrar':'POST',
-    '/veiculo/listar':'GET',
-    '/veiculo/buscar/:placa':'GET',
-    '/veiculo/atualizar/:placa':'PUT',
-    '/veiculo/excluir/:placa':'DELETE',
-    '/estadia/cadastrar':'POST',
-    '/estadia/listar':'GET',
-    '/estadia/buscar/:id':'GET',
-    '/estadia/atualizar/:id':'PUT',
-    '/estadia/excluir/:id':'DELETE',
-  ] });
+const rotas = {
+  message: "API estacionamento online",
+  rotas: [
+    { rota: "/", verbo: "GET" },
+    { rota: "/veiculo/cadastrar", verbo: "POST" },
+    { rota: "/veiculo/listar", verbo: "GET" },
+    { rota: "/veiculo/buscar/:placa", verbo: "GET" },
+    { rota: "/veiculo/atualizar/:placa", verbo: "PUT" },
+    { rota: "/veiculo/excluir/:placa", verbo: "DELETE" },
+    { rota: "/estadia/cadastrar", verbo: "POST" },
+    { rota: "/estadia/listar", verbo: "GET" },
+    { rota: "/estadia/buscar/:id", verbo: "GET" },
+    { rota: "/estadia/atualizar/:id", verbo: "PUT" },
+    { rota: "/estadia/excluir/:id", verbo: "DELETE" }
+  ]
+}
+
+app.get("/", (req, res) => {
+  res.json(rotas);
 });
 
 module.exports = app;
